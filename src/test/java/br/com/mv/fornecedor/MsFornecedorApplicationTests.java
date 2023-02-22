70399ff20e654eb7a027b18dd8b9169a520cf6bd
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-22 01:20:48 -0300

    Criacao da estrutura do projeto

---
+++ src/test/java/br/com/mv/fornecedor/MsFornecedorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mv.fornecedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFornecedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
